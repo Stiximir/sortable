150e80f8ac9535c12ad5bf105ea24d0eafb3e980
Stiximir <84101101+Stiximir@users.noreply.github.com>
2025-03-16 13:56:24 +0100

    Page working

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const htmlBody = document.getElementById("body");
+const valuePage = document.getElementById("pageSize")
+let page = 0
+let heroesTable = []
 // This function is called only after the data has been fetched, and parsed.
 const loadData = (heroes) => {
     console.log(heroes);
+    console.log(valuePage.value)
     htmlBody.innerHTML = "";
 
     for (let index = 0; index < heroes.length; index++) {
+        page = page +1
+        if (page > valuePage.value) {
+            break;
+        }
         let hero = heroes[index]
         const row = document.createElement("tr");
         
@@ -24,8 +32,14 @@ const loadData = (heroes) => {
         htmlBody.appendChild(row);
     }
 };
+
+valuePage.addEventListener("change", (event) => {
+    console.log(event.target.value);
+    page = 0
+    loadData(heroesTable)
+});
   
   // Request the file with fetch, and the data will be downloaded to your browser cache.
   fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
     .then((response) => response.json()) // parse the response from JSON
-    .then(loadData); // .then will call the `loadData` function with the JSON value.
+    .then(data => {heroesTable = data; loadData(heroesTable)}); // .then will call the `loadData` function with the JSON value.
